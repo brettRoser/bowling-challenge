1663acdb56707f4f6325a04dbb693cfb953fab2d
brettRoser <roser.brett@gmail.com>
2021-06-13 21:43:49 -0500

    Added jquery library with button disable test

--- src/js/scorer.js
+++ src/js/scorer.js
@@ -1,1 +1,14 @@
-alert("onload event - alert loaded from js");
+// alert("onload event - alert loaded from js");
+
+// $('document').ready(function() {
+//     $('.printLink').click(function() {
+//         $(this).addClass('jqueryTest');
+//     });
+// });
+
+$('document').ready(function() {
+    $('.printBtn').click(function() {
+        console.log('disabling print button');
+        $(this).attr('disabled', true);
+    });
+});
