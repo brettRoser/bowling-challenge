0919f466bc64563008cf9d1a0cb219cee300c786
brettRoser <roser.brett@gmail.com>
2021-06-22 22:00:24 -0500

    Added ability to add or remove games from scorecard

--- src/js/scorer.js
+++ src/js/scorer.js
@@ -1,6 +1,29 @@
 $('document').ready(function () {
-  $('#bowlers').change(function() {
-  var numberOfPlayers = document.getElementById('bowlers')
-  console.log(numberOfPlayers.value)
+  $('#bowlers').change(function () {
+    const scorecard = document.getElementById('scorecard')
+    const numberOfBowlers = document.getElementById('bowlers').value
+    const currentGameCount = scorecard.getElementsByTagName('table').length
+
+    if (currentGameCount <= numberOfBowlers) {
+      addGamesToScorecard(numberOfBowlers, currentGameCount, scorecard)
+    } else {
+      removeGamesFromScorecard(numberOfBowlers, currentGameCount)
+    }
   })
 })
+
+function addGamesToScorecard (numberOfBowlers, currentGameCount, scorecard) {
+  for (let i = currentGameCount; i < numberOfBowlers; i++) {
+    const initialGame = document.getElementsByTagName('table')[0]
+
+    const initialGameClone = initialGame.cloneNode(true)
+    $(initialGameClone).find('input').val('')
+    scorecard.appendChild(initialGameClone)
+  }
+}
+
+function removeGamesFromScorecard (numberOfBowlers, currentGameCount) {
+  for (let i = currentGameCount; i > numberOfBowlers; i--) {
+    document.getElementsByTagName('table')[i - 1].remove()
+  }
+}
